95fded570d04e0171d3aa6accff7ba8d01211c42
MarcosSilva13 <marcosslp45@gmail.com>
2023-08-08 19:22:55 -0300

    feat: initial commit

---
+++ src/main/java/com/digitalmenu/digitalmenuapi/DigitalmenuApiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalmenu.digitalmenuapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalmenuApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalmenuApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalmenu/digitalmenuapi/DigitalmenuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalmenu.digitalmenuapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalmenuApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
