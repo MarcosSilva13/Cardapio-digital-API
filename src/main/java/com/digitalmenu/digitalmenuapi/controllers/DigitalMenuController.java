53dd907333fbda86f48e1b24eb7e35e8718aef0b
MarcosSilva13 <marcosslp45@gmail.com>
2023-08-10 21:10:27 -0300

    feat: add controller

---
+++ src/main/java/com/digitalmenu/digitalmenuapi/controllers/DigitalMenuController.java
@@ -0,0 +1,31 @@
+package com.digitalmenu.digitalmenuapi.controllers;
+
+import com.digitalmenu.digitalmenuapi.dtos.DigitalMenuRequestDTO;
+import com.digitalmenu.digitalmenuapi.dtos.DigitalMenuResponseDTO;
+import com.digitalmenu.digitalmenuapi.services.DigitalMenuService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/menu")
+public class DigitalMenuController {
+
+    private final DigitalMenuService digitalMenuService;
+
+    public DigitalMenuController(DigitalMenuService digitalMenuService) {
+        this.digitalMenuService = digitalMenuService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DigitalMenuResponseDTO>> getAllDigitalMenu() {
+        return ResponseEntity.ok().body(digitalMenuService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<DigitalMenuResponseDTO> saveDigitalMenu(@RequestBody DigitalMenuRequestDTO requestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(digitalMenuService.save(requestDTO));
+    }
+}
