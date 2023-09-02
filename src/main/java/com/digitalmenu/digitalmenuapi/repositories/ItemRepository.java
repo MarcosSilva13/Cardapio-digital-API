bfb8a11cfef026f60d1c1ac66567805795729ef5
MarcosSilva13 <marcosslp45@gmail.com>
2023-09-02 16:56:15 -0300

    feat: add method searchAll

--- src/main/java/com/digitalmenu/digitalmenuapi/repositories/ItemRepository.java
+++ src/main/java/com/digitalmenu/digitalmenuapi/repositories/ItemRepository.java
@@ -2,9 +2,14 @@ package com.digitalmenu.digitalmenuapi.repositories;
 
 import com.digitalmenu.digitalmenuapi.entities.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ItemRepository extends JpaRepository<Item, String> {
 
+    @Query(value = "SELECT obj FROM Item obj JOIN FETCH obj.category")
+    List<Item> searchAll();
 }
