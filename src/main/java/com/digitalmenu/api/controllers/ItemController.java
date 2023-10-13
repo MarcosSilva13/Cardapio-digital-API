6218dd83975db3863ea90b05ddcafe3d6e13cd43
MarcosSilva13 <marcosslp45@gmail.com>
2023-10-13 08:44:06 -0300

    refactor: rename uri from /menu to /item

--- src/main/java/com/digitalmenu/api/controllers/ItemController.java
+++ src/main/java/com/digitalmenu/api/controllers/ItemController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/menu")
+@RequestMapping(value = "/item")
 public class ItemController {
 
     private final ItemService itemService;
