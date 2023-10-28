36701d5886008c7a5b524252d48cb148bdf9818f
MarcosSilva13 <marcosslp45@gmail.com>
2023-10-28 14:23:07 -0300

    feat: add documentation for Category

--- src/main/java/com/digitalmenu/api/controllers/CategoryController.java
+++ src/main/java/com/digitalmenu/api/controllers/CategoryController.java
@@ -3,6 +3,10 @@ package com.digitalmenu.api.controllers;
 import com.digitalmenu.api.dtos.CategoryRequestDTO;
 import com.digitalmenu.api.dtos.CategoryResponseDTO;
 import com.digitalmenu.api.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +16,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
+@Tag(name = "Category")
 public class CategoryController {
 
     private final CategoryService categoryService;
@@ -20,22 +25,42 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @Operation(summary = "Get All Categories", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success")
+    })
     @GetMapping
     public ResponseEntity<List<CategoryResponseDTO>> getAllCategory() {
         return ResponseEntity.ok().body(categoryService.getAll());
     }
 
+    @Operation(summary = "Save category", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     @PostMapping
     public ResponseEntity<CategoryResponseDTO> saveCategory(@RequestBody @Valid CategoryRequestDTO requestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.save(requestDTO));
     }
 
+    @Operation(summary = "Update category", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Data not found")
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<CategoryResponseDTO> updateCategory(@PathVariable String id,
                                                               @RequestBody @Valid CategoryRequestDTO requestDTO) {
         return ResponseEntity.ok().body(categoryService.update(id,requestDTO));
     }
 
+    @Operation(summary = "Delete category", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No content"),
+            @ApiResponse(responseCode = "404", description = "Data not found")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteCategory(@PathVariable String id) {
         categoryService.delete(id);
