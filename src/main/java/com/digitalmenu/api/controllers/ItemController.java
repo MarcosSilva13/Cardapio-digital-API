b95d152a5d8989f78f1a24a8dafaf0cc6948ad16
MarcosSilva13 <marcosslp45@gmail.com>
2023-10-28 14:16:22 -0300

    feat: add documentation for Item

--- src/main/java/com/digitalmenu/api/controllers/ItemController.java
+++ src/main/java/com/digitalmenu/api/controllers/ItemController.java
@@ -3,6 +3,11 @@ package com.digitalmenu.api.controllers;
 import com.digitalmenu.api.dtos.ItemRequestDTO;
 import com.digitalmenu.api.dtos.ItemResponseDTO;
 import com.digitalmenu.api.services.ItemService;
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/item")
+@Tag(name = "Item")
 public class ItemController {
 
     private final ItemService itemService;
@@ -20,22 +26,43 @@ public class ItemController {
         this.itemService = itemService;
     }
 
+
+    @Operation(summary = "Get All Items", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success")
+    })
     @GetMapping
     public ResponseEntity<List<ItemResponseDTO>> getAllItems() {
         return ResponseEntity.ok().body(itemService.getAll());
     }
 
+    @Operation(summary = "Save item", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "400", description = "Bad request")
+    })
     @PostMapping
     public ResponseEntity<ItemResponseDTO> saveItem(@RequestBody @Valid ItemRequestDTO requestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(itemService.save(requestDTO));
     }
 
+    @Operation(summary = "Update item", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "404", description = "Data not found")
+    })
     @PutMapping(value = "/{id}")
     public ResponseEntity<ItemResponseDTO> updateItem(@PathVariable String id,
                                                              @RequestBody @Valid ItemRequestDTO requestDTO) {
         return ResponseEntity.ok().body(itemService.update(id, requestDTO));
     }
 
+    @Operation(summary = "Delete item", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "No content"),
+            @ApiResponse(responseCode = "404", description = "Data not found")
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteItem(@PathVariable String id) {
         itemService.delete(id);
