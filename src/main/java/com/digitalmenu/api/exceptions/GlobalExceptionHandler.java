0a773e00e6cc4d99c6705afc8aaf5ea877687e3f
MarcosSilva13 <marcosslp45@gmail.com>
2023-12-02 20:05:39 -0300

    refactor: changes to use ProblemDetail class in exceptions

--- src/main/java/com/digitalmenu/api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/digitalmenu/api/exceptions/GlobalExceptionHandler.java
@@ -1,65 +1,42 @@
 package com.digitalmenu.api.exceptions;
 
-import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.*;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.stream.Collectors;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    private ExceptionDetails details = new ExceptionDetails();
+    @ExceptionHandler(EntityNotFoundException.class)
+    ProblemDetail itemNotFoundExceptionHandler(EntityNotFoundException ex) {
+        ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+        problem.setProperty("TimeStamp", LocalDateTime.now());
 
-    @ExceptionHandler(ItemNotFoundException.class)
-    public ResponseEntity<ExceptionDetails> itemNotFoundExceptionHandler(ItemNotFoundException ex,
-                                                                         HttpServletRequest request) {
-        details.setTimestamp(LocalDateTime.now());
-        details.setStatus(HttpStatus.NOT_FOUND.value());
-        details.setError(HttpStatus.NOT_FOUND.name());
-        details.setMessage(ex.getMessage());
-        details.setPath(request.getRequestURI());
-
-        return ResponseEntity.status(details.getStatus()).body(details);
+        return problem;
     }
 
-    @ExceptionHandler(CategoryNotFoundException.class)
-    public ResponseEntity<ExceptionDetails> categoryNotFoundExceptionHandler(CategoryNotFoundException ex,
-                                                                             HttpServletRequest request) {
-        details.setTimestamp(LocalDateTime.now());
-        details.setStatus(HttpStatus.NOT_FOUND.value());
-        details.setError(HttpStatus.NOT_FOUND.name());
-        details.setMessage(ex.getMessage());
-        details.setPath(request.getRequestURI());
+    @Override
+    public ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers,
+                                                               HttpStatusCode status, WebRequest request) {
+        List<String> fieldsMessage = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(FieldError::getDefaultMessage)
+                .toList();
 
-        return ResponseEntity.status(details.getStatus()).body(details);
-    }
+        ProblemDetail problem = ProblemDetail.forStatus(ex.getStatusCode());
+        problem.setDetail("Campos não preenchidos corretamente.");
+        problem.setProperty("timeStamp", LocalDateTime.now());
+        problem.setProperty("fieldsMessage", fieldsMessage);
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ValidationExceptionDetails> methodArgumentNotValidExceptionHandler(
-            MethodArgumentNotValidException ex, HttpServletRequest request) {
-
-        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
-
-        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
-
-        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
-
-        var validationDetails = new ValidationExceptionDetails();
-        validationDetails.setTimestamp(LocalDateTime.now());
-        validationDetails.setStatus(ex.getStatusCode().value());
-        validationDetails.setError(HttpStatus.valueOf(ex.getStatusCode().value()).name());
-        validationDetails.setMessage("Campos não preenchidos corretamente.");
-        validationDetails.setPath(request.getRequestURI());
-        validationDetails.setFields(fields);
-        validationDetails.setFieldsMessage(fieldsMessage);
-
-        return ResponseEntity.status(validationDetails.getStatus()).body(validationDetails);
+        return ResponseEntity.status(problem.getStatus()).body(problem);
     }
 }
